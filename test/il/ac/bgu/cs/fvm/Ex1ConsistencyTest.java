2a756cc0a987fd5273b34240b8859ea6f4bfe8db
Michael Bar-Sinai <mich.barsinai@gmail.com>
2016-11-13 22:34:15 +0200

    Formatting, updates

--- test/il/ac/bgu/cs/fvm/Ex1ConsistencyTest.java
+++ test/il/ac/bgu/cs/fvm/Ex1ConsistencyTest.java
@@ -12,7 +12,7 @@ import il.ac.bgu.cs.fvm.transitionsystem.Transition;
 import il.ac.bgu.cs.fvm.transitionsystem.TransitionSystem;
 
 /**
- * Testing the consistency tests of a transition system.
+ * Testing the consistency of a transition system implementation.
  * @author michael
  */
 public class Ex1ConsistencyTest {
@@ -24,31 +24,31 @@ public class Ex1ConsistencyTest {
         sut = FvmFacade.createInstance();
     }
     
-    @Test(expected = FVMException.class, timeout = 2000 )
+    @Test(expected=FVMException.class, timeout=2000)
     public void transitionToNonexistentState() {
         TransitionSystem<Integer, String, String> ts = makeLinearTs(5);
         ts.addTransition( new Transition<>(1, "a2", 700) );
     }
 
-    @Test(expected = FVMException.class, timeout = 2000 )
+    @Test(expected=FVMException.class, timeout=2000)
     public void transitionFromNonexistentState() {
         TransitionSystem<Integer, String, String> ts = makeLinearTs(5);
         ts.addTransition( new Transition<>(700, "a2", 1));
     }
     
-    @Test(expected = FVMException.class, timeout = 2000 )
+    @Test(expected=FVMException.class, timeout=2000)
     public void transitionWithNonexistentAction() {
         TransitionSystem<Integer, String, String> ts = makeLinearTs(5);
         ts.addTransition( new Transition<>(1, "not-an-action", 2));
     }
     
-    @Test(expected = InvalidInitialStateException.class, timeout = 2000 )
+    @Test(expected=InvalidInitialStateException.class, timeout=2000)
     public void illegalInitialState() {
         TransitionSystem<Integer, String, String> ts = makeLinearTs(5);
         ts.addInitialState( 700 );
     }
     
-    @Test( expected=InvalidLablingPairException.class, timeout = 2000 )
+    @Test(expected=InvalidLablingPairException.class, timeout=2000)
     public void illegalLabel() {
         TransitionSystem<Integer, String, String> ts = makeLinearTs(4);
         ts.addAtomicProposition("g");
